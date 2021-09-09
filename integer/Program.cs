fde21eea884f5df0b5d1aa4ac17e955fdb30ce65
Omirzakmaks <maksat.omirzak9410@gmail.com>
2021-09-09 12:16:14 +0500

    int4

--- integer/Program.cs
+++ integer/Program.cs
@@ -8,8 +8,39 @@ namespace integer
 {
     class Program
     {
+        static void int1()
+        {
+            Console.Write("uzyndyk cm engiz: L= ");
+            int L = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{L/100}metr");
+        }
+
+        static void int2()
+        {
+            Console.Write("massa kg engiz: M= ");
+            int L = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{L / 1000}tonna");
+        }
+
+        static void int3()
+        {
+            Console.Write(" razmer file Byte engiz: B= ");
+            int L = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{L / 1024} kiloByte");
+        }
+        static void int4()
+        {
+            Console.Write("Сандар (A>B) шарт бойынша енгиз: A= ");
+            int A = int.Parse(Console.ReadLine());
+            Console.Write("B= ");
+            int B = int.Parse(Console.ReadLine());
+                Console.WriteLine($"{A/B}");   
+        }
+
         static void Main(string[] args)
         {
+            int4();
+            Console.ReadKey();
         }
     }
 }
